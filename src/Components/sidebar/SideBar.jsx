77cf6208b689c7d3dc5d03fc10c9b41491a80ddb
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-13 18:11:23 +0530

    sideBar added

--- src/Components/sidebar/SideBar.jsx
+++ src/Components/sidebar/SideBar.jsx
@@ -2,7 +2,41 @@ import React from "react";
 import "./SideBar.css";
 
 const SideBar = () => {
-  return <div className="slideBar">SideBar</div>;
+  return (
+    <div className="sideBar">
+      <div className="sideBarItem">
+        <span className="sideBarTitle">ABOUT ME</span>
+        <img
+          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          alt=""
+        />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim at
+          quasi cupiditate.
+        </p>
+      </div>
+      <div className="sideBarItem">
+        <span className="sideBarTitle">CATEGORIES</span>
+        <ul className="sideBarList">
+          <li className="sideBarListItem">Life</li>
+          <li className="sideBarListItem">Music</li>
+          <li className="sideBarListItem">Style</li>
+          <li className="sideBarListItem">Sport</li>
+          <li className="sideBarListItem">Tech</li>
+          <li className="sideBarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sideBarItem">
+        <span className="sideBarTitle">FOLLOW US</span>
+        <div className="sideBarSocial">
+          <i className="sideBarIcon fab fa-facebook-square"></i>
+          <i className="sideBarIcon fab fa-instagram-square"></i>
+          <i className="sideBarIcon fab fa-pinterest-square"></i>
+          <i className="sideBarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
