9ab49b1b54c750d5ab7e12eeb7146ad0a32dad21
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-13 09:44:01 +0530

    React FlexBox

---
+++ src/Components/sidebar/SideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SideBar.css";
+
+const SideBar = () => {
+  return <div className="slideBar">SideBar</div>;
+};
+
+export default SideBar;
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Post from "../../Components/posts/Post";
+import SideBar from "../../Components/sidebar/SideBar";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="home">
+        <Post />
+        <SideBar />
+      </div>
+    </>
   );
 };
 
