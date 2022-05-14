0dd94bd7b02602b42749c096533a8091478e4e11
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-14 09:47:57 +0530

    Register Page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TopBar from "./Components/topbar/TopBar";
 import Home from "./Pages/home/Home";
 import Login from "./Pages/login/Login";
+import Register from "./Pages/Reg/Register";
 import Settings from "./Pages/settings/Settings";
 import Single from "./Pages/single/Single";
 import Write from "./Pages/write/Write";
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
     </>
   );
 }
